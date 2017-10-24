fdd152b25c258b4464b4d0b8490e11fdaed630df
MizukiFurusawa <fmzk0326@gmail.com>
2017-10-24 18:47:55 +0900

    Glideで画像を表示できるようにした

--- app/src/main/java/com/fmzk/dev/wantedlyvisit/ItemListAdapter.kt
+++ app/src/main/java/com/fmzk/dev/wantedlyvisit/ItemListAdapter.kt
@@ -7,7 +7,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kotterknife.bindView
 
 class ItemListAdapter(private val items: List<JobDetail>, private val itemClick: (String) -> Unit) :
@@ -30,11 +32,20 @@ class ItemListAdapter(private val items: List<JobDetail>, private val itemClick:
         private val jobTitle: TextView by bindView(R.id.job_title)
         private val company: TextView by bindView(R.id.company)
         private val lookingFor: TextView by bindView(R.id.looking_for)
+        private val jobImage: ImageView by bindView(R.id.job_image)
 
         fun setUp(jobDetail: JobDetail) {
             this.jobTitle.text = jobDetail.title
             this.company.text = jobDetail.company.name
             this.lookingFor.text = jobDetail.looking_for
+
+            Glide.with(jobImage.context)
+                    .load(jobDetail.image.original)
+                    .placeholder(android.R.drawable.ic_delete)
+                    .error(android.R.drawable.ic_delete)
+                    .dontAnimate()
+                    .into(jobImage)
+
             this.itemView.setOnClickListener { itemClick(jobDetail.title) }
         }
     }
