5c2895f76c9209a02b0121163f69c69ed5039c1c
MizukiFurusawa <fmzk0326@gmail.com>
2017-10-29 02:20:07 +0900

    詳細画面のデザインを修正
    画面下部のアクションボタンのデザインを修正した

--- app/src/main/java/com/fmzk/dev/wantedlyvisit/DetailActivity.kt
+++ app/src/main/java/com/fmzk/dev/wantedlyvisit/DetailActivity.kt
@@ -1,17 +1,17 @@
 package com.fmzk.dev.wantedlyvisit
 
 
-
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import kotterknife.bindView
 
 import com.google.gson.Gson
-
-
 
 
 class DetailActivity : AppCompatActivity() {
@@ -22,6 +22,9 @@ class DetailActivity : AppCompatActivity() {
     private val lookingFor: TextView by bindView(R.id.description_looking_for)
     private val backdrop: ImageView by bindView(R.id.backdrop)
     private val companyIcon: ImageView by bindView(R.id.company_icon)
+    private val bookmark: Button by bindView(R.id.button_bookmark)
+    private val candidate: Button by bindView(R.id.button_candidate)
+    private val share: FloatingActionButton by bindView(R.id.fab_share)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +51,24 @@ class DetailActivity : AppCompatActivity() {
                     .load(data.jobDetail.company.avatar.original)
                     .error(android.R.drawable.ic_delete)
                     .into(companyIcon)
+        }
+
+        bookmark.setOnClickListener { view ->
+            Snackbar.make(view, "「あとで見る」に登録されました", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show()
+        }
+
+        candidate.setOnClickListener { view ->
+            Snackbar.make(view, "「話を聞きにいきたい」が押されました", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show()
+        }
+
+        share.setOnClickListener { view ->
+            Snackbar.make(view, "「SNS共有」が押されました", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show()
         }
     }
 }
